fe9836c1ca3dbf71c90770b6143f0f02da44d75f
Ramazan Güçlü <ramazanguclu33@gmail.com>
2018-03-09 17:43:23 +0300

    mongoose connection

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
 app.get('/', (req, res) => {
