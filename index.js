bbc6add6118aff6b8a46f66951f3c4848ecbdcc1
Ramazan Güçlü <ramazang@zh.corp>
2018-05-15 16:34:44 +0300

    define static for production

--- index.js
+++ index.js
@@ -37,6 +37,24 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/dictionaryRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+
+    // express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(
+            __dirname,
+            'client',
+            'build',
+            'index.html'
+        ));
+    });
+};
+
 const PORT = process.env.PORT || 1000;
 app.listen(PORT, () => {
     console.log('listening port:', PORT);
