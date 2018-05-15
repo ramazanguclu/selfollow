9a369123db22d0087ff0bc3007e07b521cbb4e8d
Ramazan Güçlü <ramazang@zh.corp>
2018-05-15 16:46:50 +0300

    edit dev config file

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_SECRET_ID
+    googleClientSecret: process.env.GOOGLE_SECRET_ID,
+    cookieKey: 'tyuhjjhjkkllklkjjkgffghjklkkjkjhghjjkkljkl'
 }
