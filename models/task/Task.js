96f09a67f770650bbc94c2b2e2a4fd2369eb3e27
ramazanguclu <ramazanguclu33@gmail.com>
2018-06-01 17:31:52 +0300

    task log

--- models/task/Task.js
+++ models/task/Task.js
@@ -3,11 +3,11 @@ const { Schema } = mongoose;
 
 const taskSchema = new Schema({
     name: String,
+    state: { type: String, default: 'end' },
     description: { type: String, default: '' },
     _category: { type: Schema.Types.ObjectId, ref: 'TaskCategory' },
     _user: { type: Schema.Types.ObjectId, ref: 'User' },
-    total: { type: Number, default: 0 },
-    logs: [{ type: Schema.Types.ObjectId, ref: 'TaskLog' }]
+    total: { type: Number, default: 0 }
 }, { versionKey: false });
 
 mongoose.model('tasks', taskSchema);
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -7,9 +7,7 @@ const requireLogin = require('../middlewares/requirelogin');
 
 module.exports = app => {
     //#region TaskCategory
-
     const getTaskCategories = (id) => {
-        console.log(1234);
         return TaskCategory.find({ _user: id });
     };
 
@@ -47,11 +45,9 @@ module.exports = app => {
             res.status(422);
         }
     });
-
     //#endregion
 
-    //region Task
-
+    //#region Task
     const getTaskList = (id) => {
         return Task.find({ _user: id });
     };
@@ -65,6 +61,27 @@ module.exports = app => {
         });
     };
 
+    const taskById = (id) => {
+        return Task.findById(id);
+    };
+
+    const updateTaskState = (id, state) => {
+        return Task.findByIdAndUpdate(id, {
+            state: state
+        });
+    };
+
+    const updateTotalLog = (id, duration) => {
+        return Task.findByIdAndUpdate(id, {
+            $inc: { total: duration }
+        });
+    };
+
+    //task view
+    app.get('/api/task/:taskid', async (req, res) => {
+        res.send(await taskById(req.params.taskid));
+    });
+
     //task list
     app.get('/api/task_list', requireLogin, async (req, res) => {
         res.send(await getTaskList(req.user.id));
@@ -77,12 +94,12 @@ module.exports = app => {
 
     //task create
     app.post('/api/task/new', requireLogin, async (req, res) => {
-        const { name, description, categoryId } = req.body;
+        const { name, description, _category } = req.body;
 
         const task = new Task({
             name,
             description,
-            _category: categoryId,
+            _category,
             _user: req.user.id
         });
 
@@ -93,7 +110,70 @@ module.exports = app => {
             res.status(422);
         }
     });
+    //endregion
 
+    //#region TaskLog
+    //task log list
+    app.get('/api/log/list/:taskid', requireLogin, async (req, res) => {
+        res.send(await TaskLog.find({ _task: req.params.taskid }));
+    });
+
+    const updateTasklogState = async (taskId, currentState, nextState) => {
+        const log = await TaskLog.findOne({
+            _task: taskId,
+            state: currentState
+        });
+
+        const dur = (new Date()).getTime() - log.start;
+
+        try {
+            await updateTotalLog(taskId, dur);
+
+            return TaskLog.updateOne(
+                {
+                    _task: taskId,
+                    state: currentState
+                },
+                {
+                    state: nextState,
+                    end: (new Date()).getTime(),
+                    duration: dur
+                }
+            );
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    //task log create
+    app.post('/api/log/new', requireLogin, async (req, res) => {
+        const { _task, _category } = req.body;
+
+        try {
+            const { state } = await taskById(_task);
+
+            if (state === 'end') {
+                await updateTaskState(_task, 'start');
+
+                const taskLog = await new TaskLog({
+                    start: (new Date()).getTime(),
+                    state: 'start',
+                    _task,
+                    _category,
+                    _user: req.user._id
+                }).save();
+
+                res.send(taskLog);
+            } else {
+                await updateTaskState(_task, 'end');
+                await updateTasklogState(_task, 'start', 'end');
+
+                res.send({ status: 'success' });
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(422).send({ status: 'error' });
+        }
+    });
     //endregion
 };
-
