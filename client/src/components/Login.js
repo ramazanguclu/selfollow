07732dd1cde0b974201ba8769e5303c2b887235d
ramazanguclu <ramazanguclu33@gmail.com>
2018-07-06 17:52:07 +0300

    login page design

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Login extends Component {
+    componentDidMount() {
+        this.checkLogin();
+    }
+
     componentDidUpdate() {
         this.checkLogin();
     }
@@ -15,10 +19,9 @@ class Login extends Component {
     render() {
         return (
             <div>
-                <div className="divider"></div>
                 <div className="section valign-wrapper">
                     <div className="row">
-                        <a href="/auth/google">Login With Google</a>
+                        <a href="/auth/google" className="login-btn login-btn--google">Login with Google</a>
                     </div>
                 </div>
                 <div className="divider"></div>
